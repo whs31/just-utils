2b5d1837020a6e1b5be65ece463682de4debecd9
whs31 <riazantsev.dl@edu.spbstu.ru>
2025-05-28 20:00:13 +0300

    Update

--- just_utils/args.py
+++ just_utils/args.py
@@ -1,10 +1,33 @@
 import argparse
+from termcolor import colored
 
-def default_cmake_parser():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-v', '--verbose', action='store_true', help='verbose mode', default=False)
-    parser.add_argument('-c', '--clean', action='store_true', help='clean build directory', default=False)
-    parser.add_argument('-C', '--configure', action='store_true', help='reconfigure cmake only (no build)', default=False)
+
+def print_arg(name, value, color="yellow"):
+    arg = colored(f"{value}", color, attrs=["bold"])
+    print(f"{name:<25}: {arg:<25}")
+
+
+def default_cmake_parser(additional_choosers=None):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="verbose mode", default=False
+    )
+    parser.add_argument(
+        "-c",
+        "--clean",
+        action="store_true",
+        help="clean build directory",
+        default=False,
+    )
+    parser.add_argument(
+        "-C",
+        "--configure",
+        action="store_true",
+        help="reconfigure cmake only (no build)",
+        default=False,
+    )
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument(
         "-b",
@@ -44,4 +67,29 @@ def default_cmake_parser():
         dest="build",
         help="enable rwdi mode",
     )
+
+    if "qt" in additional_choosers:
+        parser.add_argument(
+            "-q",
+            "--qt",
+            choices=["5", "6"],
+            default="5",
+            help="Choose Qt version (5, 6)",
+        )
+    if "shared" in additional_choosers:
+        parser.add_argument(
+            "-s",
+            "--shared",
+            action="store_true",
+            default=True,
+        )
+        parser.add_argument("-a", "--static", action="store_false", dest="shared")
+    if "test" in additional_choosers:
+        parser.add_argument(
+            "-t",
+            "--test",
+            action="store_true",
+            help="Build tests",
+            default=False,
+        )
     return parser
